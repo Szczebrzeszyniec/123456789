8e7ccb718ee9129d83b09f14ff06ef073d8a8517
Marian Mazur <144340415+Szczebrzeszyniec@users.noreply.github.com>
2023-10-18 12:19:30 +0200

    Create magia.py

---
+++ magia.py
@@ -0,0 +1,32 @@
+# zad 1
+a = int (input())
+if (a%2 == 0):
+  print ("yuh uh")
+else:
+  print ("nuh uh")
+
+# zad 2
+a2 = int (input())
+b2 = int (input())
+c2 = int (input())
+if ((a2*a2)+(b2*b2)==c2):
+  print ("yuh uh")
+else:
+  print ("nuh uh")
+
+# zad 3
+b3=0
+a3 = int (input())
+for i in range(0,a3,1):
+  b3=b3+1
+  print("to jest",b3,"-te powtórzenie")
+
+
+# zad 4
+a4 = int (input())
+for b4 in range (a4):
+  if (b4%3==0 and b4>0):
+    print (b4)
+
+# zad 5
+a5 = int (input())
